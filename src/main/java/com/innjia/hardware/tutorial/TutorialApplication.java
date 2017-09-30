f49971ddd1559bb4ed759579b9e65063fd25d70b
edgarlau <30973784+edgarlau@users.noreply.github.com>
2017-09-30 15:18:42 +0800

    Initial commit

---
+++ src/main/java/com/innjia/hardware/tutorial/TutorialApplication.java
@@ -0,0 +1,12 @@
+package com.innjia.hardware.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialApplication.class, args);
+	}
+}
